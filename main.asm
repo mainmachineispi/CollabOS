25fa3519e2fac8fd74c6b6a15bb1f8db71b9a168
mainmachineispi <54458505+mainmachineispi@users.noreply.github.com>
2019-09-05 01:27:20 -0700

    Kernel yeet
    I have no idea if this will actually work, but I'm committing anyway.

---
+++ main.asm
@@ -0,0 +1,14 @@
+[org 0x7C00]
+main:
+    mov ah, 0x0e ; this lets us print text to the screen
+    mov al, 'O' ; we set the char we want to print
+    int 0x10 ; we print it
+    mov al, 'K'
+    int 0x10
+    
+call main ; hopefully i know enough 'bout asm and this works
+
+jmp $ ; hang
+
+times 510 - ($-$$) db 0
+dw 0xaa55
