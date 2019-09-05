0760ee38aa725e1e6eee08db5c510b70475e0e52
mainmachineispi <54458505+mainmachineispi@users.noreply.github.com>
2019-09-05 01:56:48 -0700

    Make kernel print CollabOS OK

--- main.asm
+++ main.asm
@@ -1,14 +1,15 @@
 [org 0x7C00]
 main:
-    mov ah, 0x0e ; this lets us print text to the screen
-    mov al, 'O' ; we set the char we want to print
-    int 0x10 ; we print it
-    mov al, 'K'
-    int 0x10
+    mov bx, okstring
+    call print
+    call nl
     
 call main ; hopefully i know enough 'bout asm and this works
 
 jmp $ ; hang
+
+okstring:
+    db 'CollabOS OK', 0
 
 
 %include screen.asm
