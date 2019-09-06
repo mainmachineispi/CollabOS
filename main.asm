39b555f65ce9297fac426bb5d764e5682ed343e0
mainmachineispi <54458505+mainmachineispi@users.noreply.github.com>
2019-09-05 21:06:19 -0700

    Potential fix for display bug

--- main.asm
+++ main.asm
@@ -1,11 +1,10 @@
 [org 0x7C00]
-
-mov bp, 0x9000
-mov sp, bp
-mov bx, MSG_REAL_MODE
-call print
-call switch_to_pm
-jmp $
+    mov bp, 0x9000
+    mov sp, bp
+    mov bx, MSG_REAL_MODE
+    call print
+    call switch_to_pm
+    jmp $
 
 %include "screen.asm"
 %include "gdt32.asm"
